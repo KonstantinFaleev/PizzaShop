d886aee742650addcb5e69957dc09b290fd5bb0a
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-06-02 00:42:40 +0300

    Add seed base

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_01_201159) do
+ActiveRecord::Schema.define(version: 2018_06_01_203427) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
