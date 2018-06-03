b8d592c5d09694bff8d59d0055ceba2658e6376b
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-06-04 00:23:17 +0300

    Fix function add_to_cart

--- public/app.js
+++ public/app.js
@@ -15,7 +15,7 @@ function add_to_cart(id)
 {
     var key = 'product_' + id;
 
-    var x = window.localStorage.getItem(key + id);
+    var x = window.localStorage.getItem(key);
     x = x * 1 + 1;
     window.localStorage.setItem(key, x);
     // Вывод количества item ов в корзине
