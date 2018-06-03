e86b2b386cc848ea3234e07635357e4a12d132b5
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-06-03 22:55:47 +0300

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -14,7 +14,26 @@ function something()
 function add_to_cart(id)
 {
     var key = 'product_' + id;
+
     var x = window.localStorage.getItem(key + id);
     x = x * 1 + 1;
     window.localStorage.setItem(key + id, x);
+    // Вывод количества item ов в корзине
+    alert('Item in your cart: ' + cart_get_number_of_items())
+}
+
+function cart_get_number_of_items()
+{
+    var cnt = 0;
+
+    for(var i = 0; i < window.localStorage.length; i++)
+    {
+        var key = window.localStorage.key(i);// получаем ключ
+        var value = window.localStorage.getItem(key); // получаем значение, анлог в руби: hh['bbb'] = x
+        if(key.indexOf('product_') == 0)
+        {
+          cnt = cnt + value * 1;
+        }
+    }
+    return cnt;
 }
