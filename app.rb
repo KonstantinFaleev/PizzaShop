831dc44d629a6cecddd9be264f878a129fce8d7e
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-06-04 22:45:26 +0300

    Add form to cart

--- app.rb
+++ app.rb
@@ -18,9 +18,11 @@ get '/about' do
 	erb :about
 end
 
+@o = {}
+
 post '/cart' do
-  orders_input = params[:orders]
-  @items = parse_orders_input orders_input
+  @orders_input = params[:orders]
+  @items = parse_orders_input @orders_input
 
   @items.each do |item|
     item[0] = Product.find(item[0])
