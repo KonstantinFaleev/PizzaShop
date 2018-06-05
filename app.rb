27c9bd5223972242caff5a3706beb579a75a3af0
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-06-05 19:36:32 +0300

    Display message if cart is empty

--- app.rb
+++ app.rb
@@ -25,7 +25,7 @@ post '/place_order' do
 
 	@order = Order.create params[:order]
 	#if @order.save
-		erb :order_placed
+  erb :order_placed
 	#else
 	#	@error = @order.errors.full_messages.first
 	#	erb :cart
@@ -34,18 +34,30 @@ end
 
 
 post '/cart' do
+
+  # получаем список параметров и разбираем их
   @orders_input = params[:orders_input]
   @items = parse_orders_input @orders_input
 
+  # выводим сообщение о том , что корзина пуста
+  if @items.length == 0
+    return erb :cart_is_empty
+  end
+
+  # выводим список продуктов в корзине
   @items.each do |item|
     item[0] = Product.find(item[0])
   end
+
+  # возвращаем представление по умолчанию
 	erb :cart
 end
 
 def parse_orders_input orders_input
 	s1 = orders_input.split(/,/)
+
 	arr = []
+
 	s1.each do |x|
 		s2 = x.split(/\=/)
 
